e2b3cc41c3fdc3df84c71f52a8de23009dfe7df5
etiosa obasuyi <obasuyietiosa@gmail.com>
2022-10-21 10:07:34 -0400

    remove setTimeout from login, and removed extra spaces

--- components/Login.tsx
+++ components/Login.tsx
@@ -20,26 +20,20 @@ const defaultValue: userLogin = {
 
 const Login = (props: loginProps) => {
     const [loginStatus, setLoginStatus] = useState(false)
-    const { cookie, signedUser, signoutUser } = useContext(UserContext)
+    const { cookie, signedUser } = useContext(UserContext)
 
     useEffect(() => {
-        if (props.isPremium && cookie.length > 0) {
-            setLoginStatus(false)
-        }
-        else {
+        if (props.isPremium && cookie.length <= 0) {
             setLoginStatus(true)
         }
 
-
-    },[cookie])
+        else {
+            setLoginStatus(false)
+        }
+    })
 
     const Login = () => {
-        setLoginStatus(true)
-        setTimeout(() => {
-            signedUser?.(cookies.set("token", "LOGIN", { expires: 1 / 24 }))
-        }, 600)
-
-        setLoginStatus(false);
+        signedUser?.(cookies.set("token", "LOGIN", { expires: 1 / 24 }))
     }
 
     return (
@@ -92,23 +86,7 @@ const Login = (props: loginProps) => {
                                                         px-4 py-2 text-sm font-medium text-blue-900
                                                       hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                                             onClick={Login}
-                                        >
-                                            {!loginStatus ?
-                                                <div aria-label="Loading..." role="status">
-                                                    <svg className="h-6 w-6 animate-spin" viewBox="3 3 18 18">
-                                                        <path
-                                                            className="fill-gray-200"
-                                                            d="M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19
-                                                             8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 
-                                                             3 16.9706 3 12Z"></path>
-                                                        <path
-                                                            className="fill-gray-800"
-                                                            d="M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 
-                                                            7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545
-                                                             6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z"></path>
-                                                    </svg>
-                                                </div> :
-                                                "Login"}
+                                        > Login
                                         </button>
                                     </div>
                                 </Dialog.Panel>
