a4db4f6ea4411f092ac0977acf59c8f3977af77f
Eric K <ericskao@gmail.com>
2023-03-23 23:58:29 -0700

    fix prop session type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import '../styles/index.css';
+import { Session } from 'next-auth';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
