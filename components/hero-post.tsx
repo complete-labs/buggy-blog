18a4b4a75b4eb4d3b45983397c1bf3039ebec058
Anmol Tyagi <anmolty2@gmail.com>
2023-07-16 12:39:01 -0400

    Add premium flag and blur post preview

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -1,17 +1,19 @@
-import Avatar from './avatar'
-import DateFormatter from './date-formatter'
-import CoverImage from './cover-image'
-import Link from 'next/link'
-import Author from '../types/author'
+import Avatar from "./avatar";
+import DateFormatter from "./date-formatter";
+import CoverImage from "./cover-image";
+import Link from "next/link";
+import cn from "classnames";
+import Author from "../types/author";
 
 type Props = {
-  title: string
-  coverImage: string
-  date: string
-  excerpt: string
-  author: Author
-  slug: string
-}
+  title: string;
+  coverImage: string;
+  date: string;
+  excerpt: string;
+  author: Author;
+  slug: string;
+  premium: boolean;
+};
 
 const HeroPost = ({
   title,
@@ -20,30 +22,38 @@ const HeroPost = ({
   excerpt,
   author,
   slug,
+  premium,
 }: Props) => {
   return (
-    <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage title={title} src={coverImage} slug={slug} />
-      </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-        <div>
-          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link as={`/posts/${slug}`} href="/posts/[slug]">
-              <a className="hover:underline">{title}</a>
-            </Link>
-          </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <DateFormatter dateString={date} />
-          </div>
+    <section className="relative">
+      {premium && (
+        <div className="absolute top-5 right-5 z-10">
+          <div className="bg-purple-700 text-white px-2 py-1">Premium</div>
         </div>
-        <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          <Avatar name={author.name} picture={author.picture} />
+      )}
+      <div className={cn({ "filter blur-sm": premium })}>
+        <div className="mb-8 md:mb-16">
+          <CoverImage title={title} src={coverImage} slug={slug} />
+        </div>
+        <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+          <div>
+            <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
+              <Link as={`/posts/${slug}`} href="/posts/[slug]">
+                <a className="hover:underline">{title}</a>
+              </Link>
+            </h3>
+            <div className="mb-4 md:mb-0 text-lg">
+              <DateFormatter dateString={date} />
+            </div>
+          </div>
+          <div>
+            <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+            <Avatar name={author.name} picture={author.picture} />
+          </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroPost
+export default HeroPost;
