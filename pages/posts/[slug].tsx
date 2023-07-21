5b276d9c1b92d920b67655ea9b907793a2581a32
Dan Robertson <daniel.robertson95@gmail.com>
2023-07-21 09:58:48 -0700

    Add `premium` metadata to posts

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -70,6 +70,7 @@ export async function getStaticProps({ params }: Params) {
     'content',
     'ogImage',
     'coverImage',
+    'premium',
   ])
   const content = await markdownToHtml(post.content || '')
 
